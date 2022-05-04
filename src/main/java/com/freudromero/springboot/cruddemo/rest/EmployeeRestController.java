866484055e7d05fe953ef9ef1b04fb4311438836
fred <abnertorres58@gmail.com>
2022-05-04 00:28:22 -0400

    Spring Boot - REST adding Controller Method to Delete Employee

--- src/main/java/com/freudromero/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/freudromero/springboot/cruddemo/rest/EmployeeRestController.java
@@ -57,9 +57,15 @@ public class EmployeeRestController {
         return theEmployee;
     }
 
+    // Add mapping for DELETE /employees/{employeeId} - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        if(employeeService.findById(employeeId) == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
 
+        employeeService.deleteById(employeeId);
 
-
-
-
+        return "Deleted employee id - " + employeeId;
+    }
 }
